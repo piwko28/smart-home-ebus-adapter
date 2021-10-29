9b0c75d92189396575cceb5cbf85a148e8b48a03
Szymon Piwowarczyk <szymon@piwowarczyk.net>
2021-10-29 21:23:52 +0200

    Home assistant configurations and scripts

---
+++ scripts/read-properties.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+i=0
+stdbuf -oL ebusctl find -c $1 | awk '{print $2}' > output.txt
+awk 'NF' output.txt > o.txt
+file='o.txt'
+
+while IFS= read -r line
+do
+((i=i+1))
+ebusctl read "$line" < /dev/null
+done < "$file"
+echo "Values readed from $1."
